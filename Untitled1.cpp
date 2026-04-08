1e707569d505e83a754eba8aa7c37ea65ac16c63
Sâm Võ A <linhtran27673@gmail.com>
2026-04-08 19:20:29 +0700

    firstcommit

---
+++ Untitled1.cpp
@@ -0,0 +1,23 @@
+
+  INPUT grade
+  INPUT exp
+  IF grade="E"
+    IF exp=2
+      salary = 2000
+    ELSE
+      IF exp=3
+        salary=3000
+      END IF
+    END IF
+  ELSE
+    IF grade="M"
+      IF exp=2
+        Salary=3000
+      ELSE
+        IF exp=3
+          Salary=4000
+        END IF
+      END IF
+    END IF
+  END IF
+END
