fec15ab36057e69eabd296324f4717120f4fd134
Sâm Võ A <linhtran27673@gmail.com>
2026-04-17 20:23:20 +0700

    chuyển đổi tiền tệ

--- ss008_variables_data-types_operators/Ứng dụng chuyển đổi tiền tệ/currency_conversion.js
+++ ss008_variables_data-types_operators/Ứng dụng chuyển đổi tiền tệ/currency_conversion.js
@@ -1,20 +1,20 @@
-function convert() {
-    let amount = Number(document.getElementById("amount").value);
-    let from = document.getElementById("fromCurrency").value;
-    let to = document.getElementById("toCurrency").value;
+    function convert() {
+        let amount = Number(document.getElementById("amount").value);
+        let from = document.getElementById("fromCurrency").value;
+        let to = document.getElementById("toCurrency").value;
 
-    let rate = 26000;
-    let result;
+        let rate = 26000;
+        let result;
 
-    if (from === to) {
-        result = amount;
-    }
-    else if (from === "USD") {
-        result = amount * rate;
-    }
-    else {
-        result = amount / rate;
-    }
+        if (from === to) {
+            result = amount;
+        }
+        else if (from === "USD") {
+            result = amount * rate;
+        }
+        else {
+            result = amount / rate;
+        }
 
-    console.log("Result:", result);
-}
+        document.getElementById("result").innerText = result;
+    }
