9e1905766bb9d6a0ff965010d21228eff12f5ae2
Sâm Võ A <linhtran27673@gmail.com>
2026-04-20 19:29:20 +0700

    save work

--- ss009_computer/calculate.js
+++ ss009_computer/calculate.js
@@ -1,18 +1,22 @@
+const ADD = "Addition";
+const SUB = "Subtraction";
+const MUL = "Multiplication";
+const DIV = "Division";
 function calculate(type) {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
     let result;
 
-    if (type === "Addition") {
+    if (type === ADD) {
         result = num1 + num2;
     }
-    else if (type === "Subtraction") {
+    else if (type === SUB) {
         result = num1 - num2;
     }
-    else if (type === "Multiplication") {
+    else if (type === MUL) {
         result = num1 * num2;
     }
-    else if (type === "Division") {
+    else if (type === DIV) {
         result = num1 / num2;
     }
     document.getElementById("result").innerHTML = result;
