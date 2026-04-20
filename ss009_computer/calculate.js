bb72e3a51571d9951be5fc21683baca843b9ea1f
Sâm Võ A <linhtran27673@gmail.com>
2026-04-20 18:54:55 +0700

    computer

--- ss009_computer/calculate.js
+++ ss009_computer/calculate.js
@@ -1,3 +1,8 @@
+const ADD = "Addition";
+const SUB = "Subtraction";
+const MUL = "Multiplication";
+const DIV = "Division";
+
 function calculate(type) {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
