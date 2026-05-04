f940b9df0fd7c45db300020a8f7219d8021d0298
Sâm Võ A <linhtran27673@gmail.com>
2026-05-04 15:55:19 +0700

    ss012_lesson 2

---
+++ Cấu trúc vòng lặp 1/lesson 2/calculate.js
@@ -0,0 +1,11 @@
+let temp = parseFloat(prompt("Enter current temperature:"));
+
+if (temp > 100) {
+    alert("Giảm nhiệt độ!");
+}
+else if (temp < 20) {
+    alert("Tăng nhiệt độ!");
+}
+else {
+    alert("Nhiệt độ bình thường");
+}
