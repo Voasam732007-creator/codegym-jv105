331a087b931a528a31dfea75f799981faba3234e
Sâm Võ A <linhtran27673@gmail.com>
2026-05-06 16:31:14 +0700

    lesson 3

---
+++ Cấu trúc vòng lặp 2/lesson 3/calculate.js
@@ -0,0 +1,30 @@
+const drawTriangles = () => {
+    const n = +document.getElementById("number").value;
+
+    let topLeft = "";
+    let bottomLeft = "";
+    let topRight = "";
+    let bottomRight = "";
+
+    for (let i = 1; i <= n; i++) {
+        topLeft += "*".repeat(i) + "\n";
+    }
+
+    for (let i = n; i >= 1; i--) {
+        bottomLeft += "*".repeat(i) + "\n";
+    }
+
+    for (let i = 1; i <= n; i++) {
+        topRight += " ".repeat(n - i) + "*".repeat(i) + "\n";
+    }
+
+    // 4. Dưới phải
+    for (let i = n; i >= 1; i--) {
+        bottomRight += " ".repeat(n - i) + "*".repeat(i) + "\n";
+    }
+
+    document.getElementById("topLeft").innerText = topLeft;
+    document.getElementById("bottomLeft").innerText = bottomLeft;
+    document.getElementById("topRight").innerText = topRight;
+    document.getElementById("bottomRight").innerText = bottomRight;
+};
