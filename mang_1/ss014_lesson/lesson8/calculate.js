83311225209961cd2c02d0dc7d4bcb0765838645
Sâm Võ A <linhtran27673@gmail.com>
2026-05-08 19:31:14 +0700

    lesson 8

--- mang_1/ss014_lesson/lesson8/calculate.js
+++ mang_1/ss014_lesson/lesson8/calculate.js
@@ -1,7 +1,22 @@
 const sortDescending = () => {
+
     const array = [3, 7, 12, 5, 9, 20, 15, 8, 1, 10];
 
-    array.sort((a, b) => b - a);
+    for (let i = 0; i < array.length; i++) {
 
-    result.innerText = array.join(", ");
+        for (let j = i + 1; j < array.length; j++) {
+
+            if (array[i] < array[j]) {
+
+                const temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+
+            }
+        }
+    }
+
+    const resultText = `Mảng giảm dần: ${array.join(", ")}`;
+
+    result.innerText = resultText;
 };
