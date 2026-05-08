51c2abd306599c5a7a9a68cd0ca56514d2706c9d
Sâm Võ A <linhtran27673@gmail.com>
2026-05-08 14:11:54 +0700

    lesson 1

---
+++ mang_1/ss014_lesson/lesson1/calculate.js
@@ -0,0 +1,11 @@
+const run = () => {
+    const arr = [3, 15, 7, 22, 9, 10, 11, 5, 18, 2];
+
+    let count = 0;
+
+    for (const num of arr) {
+        if (num >= 10) count++;
+    }
+
+    result.innerText = "Số phần tử >= 10: " + count;
+};
