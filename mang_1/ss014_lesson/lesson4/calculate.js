8d4430466271ebfd360feaa13def3d45912a7d64
Sâm Võ A <linhtran27673@gmail.com>
2026-05-13 20:45:37 +0700

    fix lesson 4

--- mang_1/ss014_lesson/lesson4/calculate.js
+++ mang_1/ss014_lesson/lesson4/calculate.js
@@ -1,7 +1,11 @@
 const reverseArray = () => {
     const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    const reversed = array.reverse();
+    const reversed = [];
+
+    for (let i = array.length - 1; i >= 0; i--) {
+        reversed.push(array[i]);
+    }
 
     result.innerText = "Mảng sau khi đảo: " + reversed.join(", ");
 };
