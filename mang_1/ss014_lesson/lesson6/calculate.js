65a626951e1ce030ecbbed1b3423f275a75cf4ae
Sâm Võ A <linhtran27673@gmail.com>
2026-05-13 20:42:56 +0700

    fix lesson 6

--- mang_1/ss014_lesson/lesson6/calculate.js
+++ mang_1/ss014_lesson/lesson6/calculate.js
@@ -1,11 +1,10 @@
 const findNumber = () => {
-    const array = [1, 2, 3, 4, 5, 10, 20, 8, 9, 11];
-    const v = +document.getElementById("number").value;
+    const numbers = [1, 2, 3, 4, 5, 10, 20, 8, 9, 11];
+    const inputNumber = +document.getElementById("number").value;
 
-    if (array.includes(v)) {
-        result.innerText = "V  is in the array";
-
-    }else {
-        result.innerText = "V  is not in the array";
-        }
+    if (numbers.includes(inputNumber)) {
+        result.innerText = "V is in the array";
+    } else {
+        result.innerText = "V is not in the array";
+    }
 }
